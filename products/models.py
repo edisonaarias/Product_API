fb49ba014b81733baeca8638859155d7076327bb
unknown <edisonalexanderarias@gmail.com>
2022-05-24 12:45:55 -0700

    Created products models for products app +registered products and framework in project setting

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class product(models.Model):
+    title = models.IntegerField()
+    description = models.IntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory = models.CharField(max_length=255)
