64c7b9abd65137106df26b1213b6aa4102a107a0
unknown <edisonalexanderarias@gmail.com>
2022-05-24 14:41:57 -0700

    Updated project urls + created first view function +created products app urls file

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    title = models.IntegerField()
-    description = models.IntegerField()
+    title = models.CharField(max_length=225)
+    description = models.CharField(max_length=225)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    inventory = models.CharField(max_length=255)
+    inventory_quantiy = models.IntegerField()
