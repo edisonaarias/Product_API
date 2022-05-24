fb98859f3ccccadfa3bd3184f9205b6a121bbf50
unknown <edisonalexanderarias@gmail.com>
2022-05-24 13:04:41 -0700

    Created car serializer class and file

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class product(models.Model):
+class Product(models.Model):
     title = models.IntegerField()
     description = models.IntegerField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
