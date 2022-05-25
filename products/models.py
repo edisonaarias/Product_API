8fd1da0cd34a7f995760288d5c23342a5afa89b7
unknown <edisonalexanderarias@gmail.com>
2022-05-25 11:20:24 -0700

    Registered Product model with admin site + completed post fuctionality for product + updated rest framework decimal setting

--- products/models.py
+++ products/models.py
@@ -6,4 +6,4 @@ class Product(models.Model):
     title = models.CharField(max_length=225)
     description = models.CharField(max_length=225)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    inventory_quantiy = models.IntegerField()
+    inventory_quantity = models.IntegerField()
